024d061bf563ba17556324d038714af8f926a771
Rene Olvera Romero <reneolverar@gmail.com>
2023-09-07 21:37:59 -0600

    feat: Add navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+
+import { Routes, Route } from "react-router-dom"
+
+import Nav from "./components/Nav"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Nav></Nav>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<PollsDashboard />}
+                />
+                <Route
+                    // index
+                    path="/leaderboard"
+                    element={<Leaderboard />}
+                />
+                <Route
+                    path="/login"
+                    element={<LogIn />}
+                />
+                <Route
+                    path="/newpoll"
+                    element={<NewPoll />}
+                />
+                <Route
+                    path="/poll"
+                    element={<Poll />}
+                />
+            </Routes>
     </div>
-  );
+    )
 }
 
-export default App;
+export default App
