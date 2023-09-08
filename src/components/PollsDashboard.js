596af17467a9eedbe62df6d842a1a8cc3a3d8ce8
Rene Olvera Romero <reneolverar@gmail.com>
2023-09-08 11:24:05 -0600

    feat: Connect dashboard to store

--- src/components/PollsDashboard.js
+++ src/components/PollsDashboard.js
@@ -1,97 +1,7 @@
+import { connect } from "react-redux";
 import QuestionsContainer from "./QuestionsContainer.js"
 
 const questionContainers = ["New Questions", "Done"]
-
-const questions = {
-    "New Questions": {
-        byId: {
-            q1: {
-                id: "q1",
-                author: "u1",
-                timestamp: "01.05.2023",
-                optionOne: {
-                    votes: ["u1"],
-                    text: "Example text",
-                },
-                optionTwo: {
-                    votes: ["u2"],
-                    text: "Example text",
-                },
-            },
-            q2: {
-                id: "q2",
-                author: "u2",
-                timestamp: "01.05.2023",
-                optionOne: {
-                    votes: ["u1"],
-                    text: "Example text",
-                },
-                optionTwo: {
-                    votes: ["u2"],
-                    text: "Example text",
-                },
-            },
-            q3: {
-                id: "q3",
-                author: "u3",
-                timestamp: "01.05.2023",
-                optionOne: {
-                    votes: ["u1"],
-                    text: "Example text",
-                },
-                optionTwo: {
-                    votes: ["u2"],
-                    text: "Example text",
-                },
-            },
-            q4: {
-                id: "q4",
-                author: "u4",
-                timestamp: "01.05.2023",
-                optionOne: {
-                    votes: ["u1"],
-                    text: "Example text",
-                },
-                optionTwo: {
-                    votes: ["u2"],
-                    text: "Example text",
-                },
-            },
-        },
-        allIds: ["q1", "q2", "q3", "q4"],
-    },
-    Done: {
-        byId: {
-            q5: {
-                id: "q5",
-                author: "u1",
-                timestamp: "01.05.2023",
-                optionOne: {
-                    votes: ["u1"],
-                    text: "Example text",
-                },
-                optionTwo: {
-                    votes: ["u2"],
-                    text: "Example text",
-                },
-            },
-            q6: {
-                id: "q6",
-                author: "u2",
-                timestamp: "01.05.2023",
-                optionOne: {
-                    votes: ["u1"],
-                    text: "Example text",
-                },
-                optionTwo: {
-                    votes: ["u2"],
-                    text: "Example text",
-                },
-            },
-        },
-        allIds: ["q5", "q6"],
-    },
-}
 
 export default function PollsDashboard() {
     return (
@@ -100,7 +10,6 @@ export default function PollsDashboard() {
                 <QuestionsContainer
                     key={container}
                     title={container}
-                    questions={questions[container]}
                 ></QuestionsContainer>
             ))}
         </div>
--- src/components/QuestionsContainer.js
+++ src/components/QuestionsContainer.js
@@ -1,14 +1,29 @@
 import Question from "./Question"
+import { connect } from "react-redux"
 
-export default function QuestionsContainer({ title, questions }) {
+function QuestionsContainer(props) {
     return (
         <div className="m-2 my-5">
-            <h1 className="text-2xl text-center font-bold border border-black">{title}</h1>
+            <h1 className="text-2xl text-center font-bold border border-black">
+                {props.title}
+            </h1>
             <div className="flex border border-black p-2 grid gap-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                {questions.allIds.map((id) => (
-                    <Question key={id} question={questions.byId[id]}></Question>
+                {props.sortedQuestionIds.map((id) => (
+                    <Question
+                        key={id}
+                        question={props.questions.byId[id]}
+                    ></Question>
                 ))}
             </div>
         </div>
     )
 }
+
+const mapStateToProps = ({ questions }) => ({
+    questions,
+    sortedQuestionIds: Object.keys(questions.byId).sort(
+        (a, b) => questions.byId[b].timestamp - questions.byId[a].timestamp
+    ),
+})
+
+export default connect(mapStateToProps)(QuestionsContainer)
