21084317e7dc2b8eaaff94e8ea0c995878afe3a5
Rene Olvera Romero <reneolverar@gmail.com>
2023-09-14 19:24:11 -0600

    feat: Add action handlers for questions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,12 +1,15 @@
+import { saveQuestion, saveQuestionAnswer } from "../utils/api"
+import { showLoading, hideLoading } from "react-redux-loading-bar"
+
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS"
 export const ADD_QUESTION = "ADD_QUESTION"
 export const REMOVE_QUESTION = "REMOVE_QUESTION"
 export const VOTE_QUESTION = "VOTE_QUESTION"
 
 export function receiveQuestions(questions) {
-  return {
-    type: RECEIVE_QUESTIONS,
-    questions,
+    return {
+        type: RECEIVE_QUESTIONS,
+        questions,
     }
 }
 
@@ -17,6 +20,38 @@ export function addQuestion(question) {
     }
 }
 
+export function handleAddQuestion(optionOneText, optionTwoText) {
+    return (dispatch, getState) => {
+        const { authedUser } = getState()
+        dispatch(showLoading())
+        const newQuestion = {
+            author: authedUser,
+            optionOneText,
+            optionTwoText,
+        }
+        return saveQuestion(newQuestion)
+            .catch((e) => {
+                console.warn(
+                    "Error in handleAddQuestion: ",
+                    e,
+                    ". Tried to add poll: ",
+                    newQuestion
+                )
+                alert(
+                    "There was an error adding the question, please try again"
+                )
+            })
+            .then((question) => {
+                dispatch(addQuestion(question))
+                return question
+            })
+            .then((question) => {
+                dispatch(hideLoading())
+                return question
+            })
+    }
+}
+
 export function removeQuestion(id) {
     return {
         type: REMOVE_QUESTION,
@@ -24,11 +59,22 @@ export function removeQuestion(id) {
     }
 }
 
-export function voteQuestion(questionId, option, userId) {
+function voteQuestion({ authedUser, qid, answer }) {
     return {
         type: VOTE_QUESTION,
-        questionId,
-        option,
-        userId,
+        authedUser,
+        qid,
+        answer,
+    }
+}
+
+export function handleVoteQuestion(info) {
+    return (dispatch) => {
+        dispatch(voteQuestion(info))
+        return saveQuestionAnswer(info).catch((e) => {
+            console.warn("Error in handleVoteQuestion: ", e)
+            dispatch(voteQuestion(info))
+            alert("There was an error saving the vote. Please try again")
+        })
     }
 }
